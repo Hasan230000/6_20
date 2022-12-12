95bce5fa13f2bc910783cd82eaddf8d773853fa6
Hasan230000 <120042221+Hasan230000@users.noreply.github.com>
2022-12-12 20:10:17 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg6_20;
+
+/**
+ *
+ * @author Costa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x=6,y=20;
+        System.out.println((x+y)/2);
+    }
+    
+}
